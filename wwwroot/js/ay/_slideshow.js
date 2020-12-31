e96931a52f1fe149426914b175538c6f738901ff
Michael Strange <mstrange@kloveair1.com>
2020-12-31 09:04:09 -0800

    Slide Show Script

--- wwwroot/js/ay/_slideshow.js
+++ wwwroot/js/ay/_slideshow.js
@@ -1,8 +1,26 @@
 ﻿AY.SlideShow = {
-    //total_Available_Albums: null,
-    //total_Selected_Albums: null,
+    slideCount: 0,
+    slidePosition: 0,
+    sildeTimer: 8000,
+    slideFadeTime: 1000,
 
     Init: function () {
-        console.log("Hello");
+        AY.SlideShow.slideCount = $(".home-slide-show").length;
+        AY.SlideShow.slidePosition = AY.SlideShow.slideCount;
+        setTimeout("AY.SlideShow.Step()", AY.SlideShow.sildeTimer);
+    },
+    Step: function () {
+        var lastSlide = AY.SlideShow.slidePosition;
+        if (lastSlide === 1) {
+            AY.SlideShow.slidePosition = AY.SlideShow.slideCount;
+        } else {           
+            AY.SlideShow.slidePosition = lastSlide - 1;
+        }
+        $(".home-slide-show:nth-child(" + AY.SlideShow.slidePosition + ")").fadeIn(AY.SlideShow.slideFadeTime, function () {
+            $(".home-slide-show:nth-child(" + lastSlide + ")").fadeOut(AY.SlideShow.slideFadeTime);
+        });
+        setTimeout("AY.SlideShow.Step()", AY.SlideShow.sildeTimer);
+    
     }
+
 }
