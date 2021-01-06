cab09d13d461e1c26e76f53307ac20f8e38bb742
Michael Strange <mstrange@kloveair1.com>
2021-01-05 22:30:06 -0800

    silder working

--- wwwroot/js/ay/_gallery.js
+++ wwwroot/js/ay/_gallery.js
@@ -4,14 +4,14 @@
             dots: true,
             infinite: false,
             speed: 300,
-            slidesToShow: 4,
-            slidesToScroll: 4,
+            slidesToShow: 6,
+            slidesToScroll: 6,
             responsive: [
                 {
                     breakpoint: 1024,
                     settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 3,
+                        slidesToShow: 4,
+                        slidesToScroll: 4,
                         infinite: true,
                         dots: true
                     }
@@ -19,15 +19,15 @@
                 {
                     breakpoint: 600,
                     settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2
+                        slidesToShow: 3,
+                        slidesToScroll: 3
                     }
                 },
                 {
                     breakpoint: 480,
                     settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
+                        slidesToShow: 2,
+                        slidesToScroll: 2
                     }
                 }
                 // You can unslick at a given breakpoint now by adding:
