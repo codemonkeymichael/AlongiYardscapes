19da2db5b50cf726152fb2650dc9365d12c805e3
Michael Strange <mstrange@kloveair1.com>
2021-01-05 22:31:16 -0800

    thing

--- wwwroot/js/ay/_gallery.js
+++ wwwroot/js/ay/_gallery.js
@@ -29,10 +29,7 @@
                         slidesToShow: 2,
                         slidesToScroll: 2
                     }
-                }
-                // You can unslick at a given breakpoint now by adding:
-                // settings: "unslick"
-                // instead of a settings object
+                }             
             ]
         });
     }
