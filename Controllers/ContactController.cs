eb6020a676eff5a31f9fcc56b00b0ca59dca2ef6
Michael Strange <mstrange@kloveair1.com>
2021-01-18 11:17:44 -0800

    design in progress

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using alongiYardscapes.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace alongiYardscapes.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly ILogger<AboutController> _logger;
+
+        public ContactController(ILogger<AboutController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {           
+            return View();
+        } 
+    }
+}
