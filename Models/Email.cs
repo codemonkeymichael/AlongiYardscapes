f45e63eabc3f3fbb2bbda5982a54f7ad3e3ce59e
Michael Strange <mstrange@kloveair1.com>
2021-02-06 19:44:57 -0800

    Contat Form Stub

---
+++ Models/Email.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace alongiYardscapes.Models
+{
+    public class Email
+    {
+        [Required(ErrorMessage = "Please enter your name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your e-mail.")]
+        [EmailAddress(ErrorMessage = "The E-Mail address you entered was not valid.")]
+        public string From { get; set; }
+
+        [Required(ErrorMessage = "Please enter a subject.")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Please enter a message.")]
+        public string Message { get; set; }
+
+        public bool emailSent { get; set; }
+    }
+}
